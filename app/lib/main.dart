141ed83366e12727c48a494058937c71c4adc5de
eioo <khirvola@gmail.com>
2020-03-13 14:24:36 +0200

    Add basic theming support

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,21 @@
 import 'package:eventapp/login.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(title: 'Event app demo', home: Login()));
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Event app demo',
+        home: Login(),
+        theme: ThemeData(
+            primaryColor: Colors.grey[800],
+            cardColor: Colors.grey[200],
+            hintColor: Colors.grey[550],
+            backgroundColor: Colors.white,
+            fontFamily: 'Inter',
+            textTheme: TextTheme(body1: TextStyle(color: Colors.purple)),
+    ));
+  }
+}
+
+void main() => runApp(App());
