863b559d3cc5559472954847f52f5096f9d13690
eioo <khirvola@gmail.com>
2020-03-22 15:56:58 +0200

    Add test for timeago class

---
+++ app/test/timeago_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:eventapp/timeago.dart';
+
+void main() {
+  var now = new DateTime.now();
+
+  test('Should be under minute ago', () {
+    expect(Timeago.format(now.subtract(new Duration(seconds: 30))), 'alle minuutti sitten');
+  });
+
+  test('Should be 15 minutes ago', () {
+    expect(Timeago.format(now.subtract(new Duration(minutes: 15))), '15 minuuttia sitten');
+  });
+
+  test('Should be one year ago', () {
+    expect(Timeago.format(now.subtract(new Duration(days: 365))), 'noin vuosi sitten');
+  });
+}
