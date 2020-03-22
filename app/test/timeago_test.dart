bf798e89eefb2366af85bb8b488e22778c3d6c41
eioo <khirvola@gmail.com>
2020-03-22 16:09:38 +0200

    reword

--- app/test/timeago_test.dart
+++ app/test/timeago_test.dart
@@ -4,23 +4,23 @@ import 'package:eventapp/timeago.dart';
 void main() {
   var now = new DateTime.now();
 
-  test('Should be in future', () {
+  test('is in future', () {
     expect(Timeago.format(now.subtract(new Duration(hours: -10))), 'tulevaisuudessa');
   });
 
-  test('Should be under minute ago', () {
+  test('is under minute ago', () {
     expect(Timeago.format(now.subtract(new Duration(seconds: 30))), 'alle minuutti sitten');
   });
 
-  test('Should be 15 minutes ago', () {
+  test('is 15 minutes ago', () {
     expect(Timeago.format(now.subtract(new Duration(minutes: 15))), '15 minuuttia sitten');
   });
 
-  test('Should be 3 days ago', () {
+  test('is 3 days ago', () {
     expect(Timeago.format(now.subtract(new Duration(days: 3))), '3 päivää sitten');
   });
 
-  test('Should be one year ago', () {
+  test('is one year ago', () {
     expect(Timeago.format(now.subtract(new Duration(days: 365))), 'noin vuosi sitten');
   });
 
