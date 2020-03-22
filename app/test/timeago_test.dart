bc800207ceb0dd3deebc9240b965a71a5aa99d00
eioo <khirvola@gmail.com>
2020-03-22 16:04:46 +0200

    Add future edge-case and more tests

--- app/test/timeago_test.dart
+++ app/test/timeago_test.dart
@@ -4,6 +4,15 @@ import 'package:eventapp/timeago.dart';
 void main() {
   var now = new DateTime.now();
 
+  test('Should be in future', () {
+    expect(Timeago.format(now.subtract(new Duration(hours: -10))), 'tulevaisuudessa');
+  });
+
+  test('Should be under minute ago', () {
+    expect(Timeago.format(now.subtract(new Duration(seconds: 30))), 'alle minuutti sitten');
+  });
+
+
   test('Should be under minute ago', () {
     expect(Timeago.format(now.subtract(new Duration(seconds: 30))), 'alle minuutti sitten');
   });
@@ -12,7 +21,12 @@ void main() {
     expect(Timeago.format(now.subtract(new Duration(minutes: 15))), '15 minuuttia sitten');
   });
 
+  test('Should be 3 days ago', () {
+    expect(Timeago.format(now.subtract(new Duration(days: 3))), '3 päivää sitten');
+  });
+
   test('Should be one year ago', () {
     expect(Timeago.format(now.subtract(new Duration(days: 365))), 'noin vuosi sitten');
   });
+
 }
